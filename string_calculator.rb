d0c35b114ee1a2653dfc6680ea3425a051fda97e
Bhavin Nandani <bhavin.nandani@bacancy.com>
2024-05-02 12:11:07 +0530

    Initial commit

---
+++ string_calculator.rb
@@ -0,0 +1,7 @@
+# Calculator to add numbers from a string.
+class StringCalculator
+  def add(numbers)
+    
+  end
+end
+
---
+++ string_calculator_spec.rb
@@ -0,0 +1,8 @@
+require_relative 'string_calculator'
+
+RSpec.describe StringCalculator do
+    let(:calculator) { StringCalculator.new }
+
+    describe "#add" do
+    end
+end
