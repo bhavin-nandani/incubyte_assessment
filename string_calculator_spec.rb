6d303ad6b9dbbfc504504fe130f26b1f5f356375
Bhavin Nandani <bhavin.nandani@bacancy.com>
2024-05-02 12:28:04 +0530

    added possible scenario

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -4,5 +4,46 @@ RSpec.describe StringCalculator do
     let(:calculator) { StringCalculator.new }
 
     describe "#add" do
+        context "when empty string" do
+            xit "returns 0" do
+                
+            end
+        end
+
+        context "when single number" do
+            xit "returns that number" do
+
+            end
+        end
+
+        context "when two numbers" do
+            xit "returns the sum of those numbers" do
+
+            end
+        end
+
+        context "when multiple numbers" do
+            xit "returns the sum of those numbers" do
+
+            end
+        end
+
+        context "when numbers separated by newlines" do
+            xit "returns the sum of those numbers" do
+
+            end
+        end
+
+        context "when numbers separated by different delimiters" do
+            xit "returns the sum of those numbers" do
+
+            end
+        end
+
+        context "when numbers are negative" do
+            xit "returns the sum of those numbers" do 
+                
+            end
+        end
     end
 end
